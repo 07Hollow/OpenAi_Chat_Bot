6bd2f4dc8be7dfec5cc13038c08c45b531754e66
07Hollow <sebastiansimon0727@gmail.com>
2022-12-28 22:05:22 +0100

    second commit

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     //fetch Data from Server -> bot response
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://openai-chat-bot-web-server.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
